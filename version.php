d20d72b8c369f16f5e9d1c2a123a0506f3da47c3
Luca Bösch <luca.boesch@bfh.ch>
2024-06-12 11:16:48 +0200

    Pdfannotator comment subscription like forum fixes rwthmoodle#20.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pdfannotator';
-$plugin->version   = 2025010300;
+$plugin->version   = 2025010301;
 $plugin->release  = 'PDF Annotator v1.5 release 7';
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_STABLE;
